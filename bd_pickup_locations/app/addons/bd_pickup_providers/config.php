afd683e46bfb445c249390e8017573be3bf30b9e
Zahhar Kirillov <zahhar@gmail.com>
2022-11-21 15:01:22 +0100

    Update config.php

--- bd_pickup_locations/app/addons/bd_pickup_providers/config.php
+++ bd_pickup_locations/app/addons/bd_pickup_providers/config.php
@@ -1,8 +1,8 @@
 <?php
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
-define('BD_PICKUP_PROVIDERS_SMARTPOST_FI_URL', 'http://smartpost.ee/fi_apt.json');
-define('BD_PICKUP_PROVIDERS_SMARTPOST_EE_URL', 'https://uus.smartpost.ee/places.csv');
+define('BD_PICKUP_PROVIDERS_SMARTPOST_FI_URL', 'https://smartpost.ee/fi_apt.json');
+define('BD_PICKUP_PROVIDERS_SMARTPOST_EE_URL', 'https://smartpost.ee/places.csv');
 define('BD_PICKUP_PROVIDERS_OMNIVA_URL', 'https://www.omniva.ee/locations.json');
 define('BD_PICKUP_PROVIDERS_DPD_URL', 'ftp://ftp.dpd.ee/parcelshop/psexport_latest.csv');
 define('BD_PICKUP_PROVIDERS_CARGOBUS_URL', 'cargobus.csv');
